22070c5fadfd1507db4f8fe4d30047f8502ebdef
Alejandro Martinez <alexito4@gmail.com>
2014-09-07 10:14:17 +0100

    Initial Commit

---
+++ TestCL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestCL
+//
+//  Created by Alejandro Martinez on 07/09/14.
+//  Copyright (c) 2014 Alejandro Martinez. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
